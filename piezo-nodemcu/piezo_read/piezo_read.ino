c4a545db5c909c66c109510529f3c8db2917439f
Jaehyuk Park <bn3monkey@naver.com>
2018-05-10 00:09:11 +0900

    solve the analog input problem

--- piezo-nodemcu/piezo_read/piezo_read.ino
+++ piezo-nodemcu/piezo_read/piezo_read.ino
@@ -1,7 +1,7 @@
 #define DRUM1 D0
 #define DRUM2 D1
-#define BUTTON D2
-#define THRESHOLD 10
+#define BUTTON A0
+#define THRESHOLD 50
 
 int val1;
 int val2;
@@ -15,28 +15,34 @@ void setup()
 
 void loop()
 {
+  val1 = 0;
+  val2 = 0;
+  val3 = 0;
   val1 = analogRead(DRUM1);
   val2 = analogRead(DRUM2);
   val3 = analogRead(BUTTON);
+
   
-  if(val1 > THRESHOLD)
+  if (val1)
   {
-      sprintf(buff, "DRUM1 : %d:", val1); 
-      Serial.println(buff); 
+    sprintf(buff, "DRUM1 : %d:", val1);
+    Serial.println(buff);
   }
-  
-  if(val2 > THRESHOLD)
+
+
+  if (val2)
   {
     sprintf(buff, "DRUM2 : %d:", val2);
     Serial.println(buff);
-  }
+  }  
 
 
-  if(val3)
-  {
-    sprintf(buff, "SWITCH : %d:", val3);
-    Serial.println(buff);
-  }
-
+  
+    if(val3 > THRESHOLD)
+    {
+      sprintf(buff, "BUTTON : %d:", val3);
+      Serial.println(buff);
+    } 
+  
   delay(1);
 }
