2ea3641e5d3116c7b9c84bb73bd6747528a682c9
Jaehyuk Park <bn3monkey@naver.com>
2018-05-15 03:36:50 +0900

    Add serialDataAvail

--- nodemcu-rasdberrypi/serial.cpp
+++ nodemcu-rasdberrypi/serial.cpp
@@ -9,9 +9,11 @@ int Serial_io::getSerial()
     //serialGetchar blocking 함수.. character 받을 떄까지 대기한다. 
     do
     {
-        ch = serialGetchar(serial_fd);
-        if(ch != -1)
-           buff->push(ch);
+        if(serialDataAvail(serial_fd))
+        {
+            ch = serialGetchar(serial_fd);
+            buff->push(ch);
+        }
     }
     while(ch == '\0');
     return 1;
