c8fd92e7b6b5a57f5741b535f21f2d2d3fdf9ef4
Jaehyuk Park <bn3monkey@naver.com>
2018-05-16 07:24:42 +0900

    refresh settings.py

--- edrum-server/edrum_server/settings.py
+++ edrum-server/edrum_server/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'edrum_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'edrum-server-db',
+        'USER': 'bn3monkey',
+        'PASSWORD': '1q2w3e4r5t',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
