1bee661fc5319eff894418b2aef8408a2258e923
Jaehyuk Park <bn3monkey@naver.com>
2018-05-16 11:00:13 +0900

    Test

--- edrum-server/edrum_server_app/apps.py
+++ edrum-server/edrum_server_app/apps.py
@@ -3,3 +3,5 @@ from django.apps import AppConfig
 
 class EdrumServerAppConfig(AppConfig):
     name = 'edrum_server_app'
+
+
--- edrum-server/edrum_server_app/views.py
+++ edrum-server/edrum_server_app/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from rest_framework_filters import backends
+from rest_framework.response import Response
 
 from edrum_server_app.models import NoteFileModel
 from edrum_server_app.serializers import NoteFileSerializer
 #from edrum_server_app.filters import NoteFileFilter
+from edrum_server_app.pretty_request import pretty_request
 
 class NoteFileViewSet(viewsets.ModelViewSet) :
     queryset = NoteFileModel.objects.all()
@@ -12,5 +14,13 @@ class NoteFileViewSet(viewsets.ModelViewSet) :
     #filter_backends = (backends.DjangoFilterBackend,)
     #filter_class = NoteFileFilter
 
+    #def create(self, request, pk = None):
+        #f = open("Log2.txt", "w")
+        #f.write(pretty_request(request))
+        #f.close()
+        #serializer = NoteFileSerializer(data=request.data)
+        #if serializer.is_valid():
+        #    serializer.Meta.model(request.data).save()
+        #return Response()
 # Create your views here.
 
