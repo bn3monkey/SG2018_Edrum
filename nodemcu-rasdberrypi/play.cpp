146088fb29462719ce8a1d835da144e4ed6eb850
Jaehyuk Park <bn3monkey@naver.com>
2018-06-04 03:27:27 +0900

    add file_initialize

--- nodemcu-rasdberrypi/play.cpp
+++ nodemcu-rasdberrypi/play.cpp
@@ -1,5 +1,14 @@
 #include "play.hpp"
 #include "serial_protocol.h"
+
+void Serial_play::deallocation()
+{
+    if(drum1 != NULL)
+        delete drum1;
+    if(drum2 != NULL)
+        delete drum2;
+}
+
 void Serial_play::play(note* temp, char* buf)
 {
     if(!playing && temp->drum == cmd_playstart)
@@ -17,6 +26,44 @@ void Serial_play::play(note* temp, char* buf)
     else if(playing && temp->drum == cmd_playend)
     {
 
+    }
+}
+
+void Serial_play::file_initialize(char* buf)
+{
+    char temp[10];
+    if(!playable)
+    {
+        printf("File cannot be opend : %s\n", current_filename);
+        return;
+    }
+
+    fp.open(current_filename);
+
+    //첫번쨰 드럼
+    fp >> temp >> drum1_cnt;
+    drum1 = new note[drum1_cnt];
+    if(drum1 == NULL)
+    {
+        fprintf(stderr, "PLAY DRUM1 ALLOCATION ERROR!\n");
+        return;
+    }
+    for(int i=0;i<drum1_cnt;i++)
+    {
+        fp >> &(drum1[i].power) >> &(drum1[i].msec);
+    }
+
+    //두번쨰 드럼
+     fp >> temp >> drum2_cnt;
+    drum2 = new note[drum2_cnt];
+    if(drum2 == NULL)
+    {
+        fprintf(stderr, "PLAY DRUM2 ALLOCATION ERROR!\n");
+        return;
+    }
+    for(int i=0;i<drum2_cnt;i++)
+    {
+        fp >> &(drum2[i].power) >> &(drum2[i].msec);
     }
 }
 
--- nodemcu-rasdberrypi/play.hpp
+++ nodemcu-rasdberrypi/play.hpp
@@ -1,4 +1,6 @@
 #include "record.h"
+#include <fstream>
+
 class Serial_play
 {
     //현재 참조하고 있는 파일의 번호.
@@ -10,6 +12,26 @@ class Serial_play
 
     //현재 채점 중인지 여부
     bool playing;
+
+    //현재 참조하고 있는 파일에서 가져온 노트 배열.
+    note* drum1;
+    int drum1_cnt;
+    note* drum2;
+    int drum2_cnt;
+
+    //현재 참조하고 있는 파일 스트림
+    ifstream fp;
+    
+    //노트 배열 할당 해제
+    void deallocation();
+
+    //현재 참고하고 있는 파일을 자료구조에 저장하고 
+    //일부 LED 정보를 buffer에 복사한다.
+    void file_initialize(char* buf);
+    //현재 참조하고 있는 파일의 번호를 올린다.
+    void fileup(char* buf);       
+    //현재 참조하고 있는 파일의 번호를 내린다.
+    void filedown(char* buf);
 
     public:
         Serial_play()
@@ -31,8 +53,5 @@ class Serial_play
         //Serial에서 들어온 문자열 명령에 따라서 필요한 함수들을 수행한다.
         void play(note* temp, char* buf);
 
-        //현재 참조하고 있는 파일의 번호를 올린다.
-        void fileup(char* buf);       
-        //현재 참조하고 있는 파일의 번호를 내린다.
-        void filedown(char* buf);
+        
 };
