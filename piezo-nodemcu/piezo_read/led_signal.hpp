a75a83a5f5f0d94f932f4f5e331c427d74669c60
Jaehyuk Park <bn3monkey@naver.com>
2018-06-05 01:47:49 +0900

    add note_queue and test

--- piezo-nodemcu/piezo_read/led_signal.hpp
+++ piezo-nodemcu/piezo_read/led_signal.hpp
@@ -1,9 +1,15 @@
-#include<arduino.h>
+#include <arduino.h>
+
+#define MAX_SIGNAL 256
+
 class LED_signal
 {
     int pin_red;
     int pin_green;
     int pin_blue;
+
+    int ledtime[MAX_SIGNAL];
+    int index_ledtime;
 
     public:
         LED_signal(int _red = 0, int _green = 1, int _blue = 2) :
@@ -14,5 +20,11 @@ class LED_signal
             pinMode(_blue, OUTPUT);
         }
         
+        void read()
         void write(int red, int green, int blue);
+
+        //파일에서 읽어와 led 불을 키기 위해, 아두이노에서 정보를 받아옴, 
+        void download();
+        //아두이노에서 일부만 받아옴.
+        void partial_download();
 };
