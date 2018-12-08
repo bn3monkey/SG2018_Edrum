ba053fb35c6faf473e12c6736fc293653b93e10a
Jaehyuk Park <bn3monkey@naver.com>
2018-12-08 21:54:17 +0900

    add hardware module

--- piezo-arduino/piezo_read/NoteData.hpp
+++ piezo-arduino/piezo_read/NoteData.hpp
@@ -5,17 +5,19 @@
 
 enum NoteProtocol
 {
+    np_none = 0x00,
     //drum 0x10 ~ 0x30
-    np_drum1 = 0x11,
-    np_drum2 = 0x12,
+    np_drum1 = 0x01,
+    np_drum2 = 0x02,
 
     //switch 0x40 ~ 0x60
-    np_up,
-    np_down,
-    np_right,
-    np_left,
-    np_ok,
-    np_idle,
+    np_up = 0x03,
+    np_down = 0x04,
+    np_right = 0x05,
+    np_left = 0x06,
+    np_ok = 0x07,
+    np_idle = 0x08,
+    np_num,
 };
 
 class NoteData
@@ -25,12 +27,14 @@ public:
     int drum;
     int power;
 
-    NoteData()
-    {
-        time = 0;
-        drum = 0;
-        power = 0;
-    }
+    NoteData(unsigned long _time = 0, int _drum = 0, int _power = 0) : time(_time), drum(_drum), power(_power)
+	{}
+	NoteData(const NoteData& e)
+	{
+		this->time = e.time;
+		this->drum = e.drum;
+		this->power = e.power;
+	}
     void send()
     {
         Serial.print(this->time ,DEC);
