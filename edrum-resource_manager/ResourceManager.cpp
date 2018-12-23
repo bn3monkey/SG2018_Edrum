8d2f480e28415e339ee8791b58e55dff350c9e53
Jaehyuk Park <bn3monkey@naver.com>
2018-12-23 21:12:04 +0900

    try to fix segmentation fault in rm

--- edrum-resource_manager/ResourceManager.cpp
+++ edrum-resource_manager/ResourceManager.cpp
@@ -18,12 +18,14 @@ bool ResourceManager::initialize(const std::string& local_path, const std::strin
     if(!list_server)
     {
         delete pCM;
+        pCM = nullptr;
         std::cout << "ERROR : SERVER_LIST ALLOCATION FAIL!" << std::endl;
         return false;
     }
     if(!list_server->initialize())
     {
         delete pCM;
+        pCM = nullptr;
         std::cout << "ERROR : SERVER_LIST INITIALIZATION FAIL!" << std::endl;
         return false;
     }
@@ -32,14 +34,18 @@ bool ResourceManager::initialize(const std::string& local_path, const std::strin
     if(!list_local)
     {
         delete pCM;
+        pCM = nullptr;
         delete list_server;
+        list_server = nullptr;
         std::cout << "ERROR : LOCAL_LIST ALLOCATION FAIL!" << std::endl;
         return false;
     }
     if(!list_local->initialize(local_path))
     {
         delete pCM;
+        pCM = nullptr;
         delete list_server;
+        list_server = nullptr;
         std::cout << "ERROR : LOCAL_LIST INITIALIZATION FAIL!" << std::endl;
         return false;
     }
@@ -48,16 +54,22 @@ bool ResourceManager::initialize(const std::string& local_path, const std::strin
     if(!list_my)
     {
         delete pCM;
+        pCM = nullptr;
         delete list_server;
+        list_server = nullptr;
         delete list_local;
+        list_local = nullptr;
         std::cout << "ERROR : MY_LIST ALLOCATION FAIL!" << std::endl;
         return false;
     }
     if(!list_my->initialize(my_path))
     {
         delete pCM;
+        pCM = nullptr;
         delete list_server;
+        list_server = nullptr;
         delete list_local;
+        list_local = nullptr;
         std::cout << "ERROR : MY_LIST INITIALIZATION FAIL!" << std::endl;
         return false;
     }
@@ -67,11 +79,27 @@ bool ResourceManager::initialize(const std::string& local_path, const std::strin
 void ResourceManager::destroy()
 {
     if(list_server != nullptr)
+    {
         delete list_server;
+        list_server = nullptr;
+    }
+    std::cout << "SERVER_LIST DEALLOCATION" << std::endl;
     if(list_local != nullptr)
+    {
         delete list_local;
+        list_local = nullptr;
+    }
+    std::cout << "LOCAL_LIST DEALLOCATION" << std::endl;
     if(list_my != nullptr)
+    {
         delete list_my;
+        list_my = nullptr;
+    }
+    std::cout << "MY_LIST DEALLOCATION" << std::endl;
     if(pCM != nullptr)
+    {
         delete pCM;
+        pCM = nullptr;
+    }
+    std::cout << "COMMUNICATION MODULE DEALLOCATION" << std::endl;
 }
