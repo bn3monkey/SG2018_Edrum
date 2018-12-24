568572e8232fa4b58abf5bb06a74e4ddbba0e900
bn3monkey <bn3monkey@naver.com>
2018-12-24 11:59:23 +0900

    console play

--- edrum-integrate/main.cpp
+++ edrum-integrate/main.cpp
@@ -1,33 +1,109 @@
-//#include "../edrum-hardware/Hardware_IO/Hardware_IO.hpp"
+#include "../edrum-hardware/Hardware_IO/Hardware_IO.hpp"
 #include "../edrum-resource_manager/ResourceManager.hpp"
 #include <memory>
+#include <thread>
+#include <chrono>
+
+unsigned long long int elapsed = 0;
+std::vector<NoteData> Notelist;
+std::mutex Notelist_mutex;
+std::vector<string> printed;
+std::mutex printed_mutex;
+int timer_toggle;
+void ClearScreen()
+{
+    std::cout << std::string( 100, '\n' );
+}
+void timer()
+{
+    while(timer_toggle)
+    {
+        elapsed++;
+        unsigned long long int note_time = Notelist.front().time;
+        note_time - elapsed;
+        if(note_time < elapsed)
+        {
+            Notelist_mutex.lock();
+            Notelist.erase(Notelist.begin());
+            Notelist_mutex.unlock();
+        }
+        ClearScreen();
+        std::cout << "note_ time : " << note_time << std::endl;
+        printed_mutex.lock();
+        for(size_t i=0;i<printed.size();i++)
+        {
+            std::cout << printed[i] << std::endl;
+        }
+        printed_mutex.unlock();
+        std::this_thread::sleep_for (std::chrono::milliseconds(1));
+    }
+}
 int main()
 {
-	//Hardware_IO::initialize();
+	Hardware_IO::initialize();
 	std::shared_ptr<ResourceManager> pRM = std::make_shared<ResourceManager>();
     pRM->initialize();
    
-    ServerList* pServer = pRM->getServerlist();
-    pServer->updatePage(0);
-    pServer->updatePage(1);
-    pServer->print();
-    pServer->download(3);
+    pLocalList->updatePage(0);
+    pLocalList->print();
 
-    std::cout << "All page : " << pServer->getMaxpage() << std::endl;
-    //pServer->downloadCancel(3);
-    //pServer->download(3);
-    
-    /*
-    LocalList* pLocal = pRM->getLocallist();
-    pLocal->updatePage(0);
-    pLocal->print();
-    */
+    std::cout << "What do you want to play?" << std::endl;
+    int number;
+    std::cin >> number;
 
-    MyList* pMy = pRM->getMylist();
-    pMy->updatePage(0);
-    pMy->print();
-    pMy->upload(3);
-    //pMy->uploadCancel(3);   
+    SongData* song = pLocalList->getSong(number); 
+    song->read(Notelist);
 
-	//Hardware_IO::destroy();
+    std::function<void()> container;
+    container = []() {
+		std::unique_lock<std::mutex> lk(m);
+		lk.unlock();
+		cv.notify_all();
+		printf("End!\n");
+	};
+	Hardware_IO::registCallback(container, np_ok);
+
+    container;
+    container = []() {
+        Notelist_mutex.lock();
+        unsigned long long int note_time = Notelist.front().time;
+        Notelist_mutex.unlock();
+        unsigned long long int time_gap = note_time - elapsed;
+        if(time_gap <= 500)
+        {
+            if(time_gap > 400)
+            {
+                std::cout << "Miss" << std::endl;
+            }
+            else if(time_gap > 300)
+            {
+                std::cout << "bad" << std::endl;
+            }
+            else if(time_gap > 200)
+            {
+                std::cout << "soso" << std::endl;
+            }
+            else if(time_gap > 100)
+            {
+                std::cout << "good" << std::endl;
+            }
+            else if(time_gap > 0)
+            {
+                std::cout << "very good" << std::endl;
+            }
+        }
+    };
+    Hardware_IO::registerCallback(container, np_drum1);
+
+	elapsed = 0;
+    timer_toggle = 1;
+    std::thread timer_thread = std::thread(timer);
+	
+	std::unique_lock<std::mutex> lk(m);
+	cv.wait(lk);
+
+    timer_toggle = 0;
+    std::thread timer_thread.join();
+
+	Hardware_IO::destroy();
 }
