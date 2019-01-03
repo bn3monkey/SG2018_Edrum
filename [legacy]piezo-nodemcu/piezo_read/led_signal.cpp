9f656b52eb1529db5df1a2b7082f35b6359e4755
bn3monkey <bn3monkey@naver.com>
2019-01-03 14:08:23 +0900

    Remove legacy modules

--- [legacy]piezo-nodemcu/piezo_read/led_signal.cpp
+++
@@ -1,8 +0,0 @@
-#include "led_signal.hpp"
-
-void LED_signal::write(int red, int green, int blue)
-{
-    analogWrite(pin_red, red);
-    analogWrite(pin_green, green);
-    analogWrite(pin_blue, blue); 
-}
